2aaa822d8d944c9ce0a648a1f9a922054b4a24c2
bch0ng <bchong@uw.edu>
2019-12-16 09:26:14 -0800

    [Add] Adds brew bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+# Brew checkup
+brew update
+brew upgrade
+
+# Workflow
+brew install tmux
+brew install vim
+brew install zsh
+
+# Tools/Utilities
+# JavaScript tools
+brew install node
+brew install npm
+brew install yarn
+
+# Containerization
+brew install docker
+
+# Languages
+brew install go
+brew install python
+brew install sass
+brew install kotlin
+
+# Databases
+brew install mongodb
+brew install mysql
+brew install postgresql
+brew install redis
+brew install sqlite
+
+# Etc.
+brew install openssl
