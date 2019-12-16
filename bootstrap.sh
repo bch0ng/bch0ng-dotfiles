9e9c5cd8b3885356c20e459e90abeb90a1b07644
bch0ng <bchong@uw.edu>
2019-12-16 09:34:12 -0800

    [Add] Adds Oh My Zsh stuff.

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,3 +31,8 @@ brew install sqlite
 
 # Etc.
 brew install openssl
+
+# ZSH setup
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
