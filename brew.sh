03ea3dabc4244df3a80a00deee968781b1d9340a
Brandon Chong <brandonchong.bchong@gmail.com>
2021-11-26 19:42:02 -0800

    update

---
+++ brew.sh
@@ -0,0 +1,21 @@
+# Brew checkup
+brew update
+brew upgrade
+
+# Workflow
+brew install tmux
+brew install vim
+brew install zsh
+
+# Tools/Utilities
+# JavaScript tools
+brew install node
+brew install npm
+brew install yarn
+
+# Containerization
+brew install docker
+
+# Etc.
+brew install openssl
+
